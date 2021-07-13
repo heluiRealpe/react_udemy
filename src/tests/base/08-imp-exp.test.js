86c70a7b20d958b09891901985863dcb51d60bf8
Héctor Realpe <hrealpe@gmail.com>
2021-07-13 14:03:19 +0200

    pruebas

--- src/tests/base/08-imp-exp.test.js
+++ src/tests/base/08-imp-exp.test.js
@@ -1,4 +1,4 @@
-import { getHeroeById } from "../../base-pruebas/08-imp-exp";
+import { getHeroeById, getHeroesByOwner } from "../../base-pruebas/08-imp-exp";
 import heroes from "../../data/heroes";
 
 
@@ -17,13 +17,40 @@ describe('Pruebas en funciones de Héroes', () => {
 
     test('debe de retornar un undefined si Héroe no existe', () => {
 
-        const id = 1;
+        const id = 10;
         const heroe = getHeroeById(id);
 
-        const heroeData = heroes.find( h => h.id === id);
-
-        expect( heroe ).toEqual( heroeData );
+        expect( heroe ).toBe( undefined );
         
     })
+
+    //  debe de retornar un arreglo con los héroes de DC
+    // toEqual al arreglo filtrado
+
+    test('debe de retornar un arreglo con los héroes de DC', () => {
+
+        const owner = 'DC';
+        const heroes = getHeroesByOwner(owner);
+
+
+        const heroesData = heroes.filter( h => h.owner === owner);
+
+        expect( heroes ).toEqual( heroesData );
+        
+    })
+
+    // debe de retornar un arreglo con los héroes de Marvel de
+    // length = 2
+
+    test('debe de retornar un arreglo con los héroes de Marvel', () => {
+        
+        const owner = 'Marvel';
+        const heroes = getHeroesByOwner(owner);
+
+        expect(heroes.length).toBe(2);
+
+    })
+    
+
 
 })
